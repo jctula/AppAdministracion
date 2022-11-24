4758860e1f27fac02fe15647ee471ac5352e32be
JC Tula <jc_tula@outlook.com>
2022-11-24 13:22:39 -0600

    Proyecto completo

---
+++ Models/Rol.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeptosES.AdministradorBlazor.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+    }
+}
